f1d8edab98cf42767d8500cc7171a62cdfe9f2ea
Souzaa <122699226+zinfex@users.noreply.github.com>
2023-07-23 23:41:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
+let lista = []
+console.log(lista)
+
+let i = 0
+
 $('#btn').on('click', function() {
+
     let tarefa = $('#task').val()
-    $('ul').append('<li>' + tarefa + '</li>')
+    lista.push(tarefa)
+
+    $('ul').append(`<li id="${i}"> ${tarefa} <div id="edit"> <i class="fa-solid fa-check" onclick="check(${i})"></i>
+    <i class="fa-solid fa-trash" onclick="trash(${i})"></i> </div> </li>`)
+
+    i = i+1
 })
 
 $('#clear').on('click', function(){
-    $('li').remove()
+    $('li').remove()    
+    lista.length = 0
 })
+
+$('.fa-solid fa-check').on('click', function(){
+    $('li').css("background-color", "green")
+})
+
+function check(indice) {
+    let li = document.querySelectorAll('li')
+    li[indice].classList.toggle('green')
+}
