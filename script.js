c996d1f3713e41e1cd175a7255d78d3c2145e5ac
Souzaa <122699226+zinfex@users.noreply.github.com>
2023-07-25 01:07:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,23 +7,30 @@ $('#btn').on('click', function() {
 
     let tarefa = $('#task').val()
     lista.push(tarefa)
-
-    $('ul').append(`<li id="${i}"> ${tarefa} <div id="edit"> <i class="fa-solid fa-check" onclick="check(${i})"></i>
-    <i class="fa-solid fa-trash" onclick="trash(${i})"></i> </div> </li>`)
-
-    i = i+1
+    listarDados()
 })
+
+function listarDados() {
+    $('#campo').html('')
+
+    lista.map((tarefa, i) => {
+        $('ul').append(`<li id="${i}"> ${tarefa} <div id="edit"> <i class="fa-solid fa-check" onclick="check(${i})"></i>
+        <i class="fa-solid fa-trash" onclick="trash(${i})"></i> </div> </li>`)
+    })
+}
 
 $('#clear').on('click', function(){
     $('li').remove()    
     lista.length = 0
 })
 
-$('.fa-solid fa-check').on('click', function(){
-    $('li').css("background-color", "green")
-})
-
 function check(indice) {
     let li = document.querySelectorAll('li')
     li[indice].classList.toggle('green')
+}
+
+function trash(indice) {
+    $('li').remove(indice)   
+    lista.splice(indice, 1)
+    listarDados()
 }
